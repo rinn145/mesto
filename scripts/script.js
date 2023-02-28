65e9ddd6e549a2a7732ca98f6abc0fe21f2d5be9
rinn145 <rt_rio@mail.ru>
2023-02-28 20:12:54 +0500

    Hotfix

--- scripts/script.js
+++ scripts/script.js
@@ -25,36 +25,20 @@ let profileSubtitle = document.querySelector(".profile__subtitle")
 userName.value = profileTitle.textContent;
 userAbout.value = profileSubtitle.textContent;
 
-const toggleSavePopup = function () {
-   profileTitle.textContent = userName.value;
-   profileSubtitle.textContent = userAbout.value;
-   popup.classList.toggle('popup_opened')
-}
 
 buttonOpenEdit.addEventListener('click', toggleOpenPopup);
 closeButton.addEventListener("click", toggleOpenPopup);
-saveButton.addEventListener("click", toggleSavePopup);
 
 
-let formElement = document.querySelector(".popup__profile-form");
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector(".popup__name");
-let jobInput = // Воспользуйтесь инструментом .querySelector()
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+let formElement = document.querySelector(".popup__form");
+
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    evt.preventDefault();
 
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    profileTitle.textContent = userName.value;
+    profileSubtitle.textContent = userAbout.value;
+    popup.classList.toggle('popup_opened')
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit); 
