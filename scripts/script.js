e57c4235165bb344fc3b97124b644c56fdf8d728
rinn145 <rt_rio@mail.ru>
2023-03-01 19:14:13 +0500

    Hotfix

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,28 @@
-const popup = document.querySelector(".popup");
-const buttonOpenEdit = document.querySelector(".profile__edit-button");
-const closeButton = popup.querySelector(".popup__close");
-const saveButton = popup.querySelector(".popup__save-button");
-const likeButton = document.querySelectorAll(".card__edit-like-button");
-
-
-
-likeButton.forEach(heart => {
-   heart.addEventListener('click', () => {
-      heart.classList.toggle('added')
-   })
-});
+const popup = document.querySelector('.popup');
+const buttonOpenEdit = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close');
+let userName = document.querySelector('#name-input');
+let userAbout = document.querySelector('#about-input');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle')
+let formElement = document.querySelector('.popup__form');
 
 
 const toggleOpenPopup = function () {
+   userName.value = profileTitle.textContent;
+   userAbout.value = profileSubtitle.textContent;
    popup.classList.toggle('popup_opened')
 };
 
-let userName = document.querySelector("#name-input");
-let userAbout = document.querySelector("#about-input");
-let profileTitle = document.querySelector(".profile__title");
-let profileSubtitle = document.querySelector(".profile__subtitle")
 
-userName.value = profileTitle.textContent;
-userAbout.value = profileSubtitle.textContent;
+function handleFormSubmit (evt) {
+   evt.preventDefault();
+   profileTitle.textContent = userName.value;
+   profileSubtitle.textContent = userAbout.value;
+   toggleOpenPopup();
+}
 
 
 buttonOpenEdit.addEventListener('click', toggleOpenPopup);
 closeButton.addEventListener("click", toggleOpenPopup);
-
-
-
-let formElement = document.querySelector(".popup__form");
-
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-
-    profileTitle.textContent = userName.value;
-    profileSubtitle.textContent = userAbout.value;
-    popup.classList.toggle('popup_opened')
-}
-
 formElement.addEventListener('submit', handleFormSubmit); 
