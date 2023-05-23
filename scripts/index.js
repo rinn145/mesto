88afcd5409efa926956aac62c18f75f6064e2a8b
rinn145 <rt_rio@mail.ru>
2023-05-23 18:18:34 +0500

    fallTry

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,12 @@ const closeButton = popup.querySelector('.popup__close');
 const closeAddButton = popupAdd.querySelector('.popup__close');
 let userName = document.querySelector('#name-input');
 let userAbout = document.querySelector('#about-input');
+let namesCards = document.querySelector('#namesCards-input');
+let urlCards = document.querySelector('#url-input');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle')
 let formElement = document.querySelector('.popup__form');
+let formElementAdd = document.querySelector('.popup__form-Add');
 
 
 const toggleOpenPopup = function () {
@@ -17,17 +20,10 @@ const toggleOpenPopup = function () {
    popup.classList.toggle('popup_opened')
 };
 
-const toggleAddPopup = function () {
-  popupAdd.classList.toggle('popup_opened')
-};
-
 const toggleClosePopup = function () {
    popup.classList.toggle('popup_opened');
 };
 
-const toggleAddClose = function () {
-   popupAdd.classList.toggle('popup_opened');
-};
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
@@ -36,12 +32,30 @@ function handleFormSubmit (evt) {
   toggleClosePopup()
 };
 
+const toggleAddPopup = function () {
+  popupAdd.classList.toggle('popup_opened')
+};
+
+const toggleAddClose = function () {
+   popupAdd.classList.toggle('popup_opened');
+};
+
+function handleFormSubmitAdd (evt) {
+  evt.preventDefault();
+  namesCards.textContent = namesCards.value;
+  urlCards.textContent = urlCards.value;
+  render( placeInfo);
+  toggleAddPopup()
+};
+
+
 
 buttonAdd.addEventListener('click', toggleAddPopup);
 buttonOpenEdit.addEventListener('click', toggleOpenPopup);
 closeButton.addEventListener("click", toggleClosePopup);
 closeAddButton.addEventListener("click", toggleAddClose);
 formElement.addEventListener('submit', handleFormSubmit); 
+formElementAdd.addEventListener('submit', handleFormSubmitAdd); 
 
 const initialCards =  [
   {
@@ -73,6 +87,7 @@ const initialCards =  [
     link: 'https://virtus-img.cdnvideo.ru/images/as-is/plain/b0/b081f035d3f1d7c36a2b2192874f3c0d.jpg@jpg'
   }
 ];
+
 
 
 const placesContainer = document.querySelector(".cards");
@@ -116,6 +131,4 @@ function removeParent(){
     let revDiv = this.parentElement;
     revDiv.remove();
 }
-
-
 
