1df0f7df67bfbe083d3b527d9cfac01cdb406ac0
rinn145 <rt_rio@mail.ru>
2023-06-13 19:28:10 +0500

    LASTCommit

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,20 +1,20 @@
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage , config ) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('popup_input-invalid')
+  inputElement.classList.add(config.popupValid);
   errorElement.textContent = errorMessage;
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement , config) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup_input-invalid')
+  inputElement.classList.remove(config.popupValid);
   errorElement.textContent = '';
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement , config) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage , config);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement , config);
   }
 };
 
@@ -28,28 +28,26 @@ function hasInvalidInput(inputList) {
 function setEventListeners(formElement, config) {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
   const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  disableSubmitButton(buttonElement);
+  disableSubmitButton(buttonElement , config);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
+      checkInputValidity(formElement, inputElement , config);
       if (hasInvalidInput(inputList)) {
-        disableSubmitButton(buttonElement);
+        disableSubmitButton(buttonElement , config );
       } else {
-        enableSubmitButton(buttonElement);
+        enableSubmitButton(buttonElement , config );
       }
     });
   });
 };
 
-const disableSubmitButton = (buttonElement) => {
-  buttonElement.classList.add('popup-button_inactive');
-  buttonElement.removeAttribute("disabled", 'disabled');
+const disableSubmitButton = (buttonElement , config) => {
+  buttonElement.classList.add(config.inactiveButtonClass);
   buttonElement.disabled = true;
 }
 
-const enableSubmitButton = (buttonElement) => {
-  buttonElement.classList.remove('popup-button_inactive');
-  buttonElement.setAttribute("disabled", 'disabled');
+const enableSubmitButton = (buttonElement , config) => {
+  buttonElement.classList.remove(config.inactiveButtonClass);
   buttonElement.disabled = false;
 }
 
@@ -68,8 +66,9 @@ enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save-button',
-  inactiveButtonClass: '.popup-button_inactive',
+  inactiveButtonClass: 'popup-button_inactive',
   inputErrorClass: '.popup__input-error',
+  popupValid: 'popup_input-invalid'
 });
 
 
